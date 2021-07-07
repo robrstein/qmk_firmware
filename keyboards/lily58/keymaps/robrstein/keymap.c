5089bf6b4d970c8fa56619ce47a38f0463953fec
robrstein <robrstein@gmail.com>
2021-07-06 22:18:31 -0400

    Update keymap.c

--- keyboards/lily58/keymaps/robrstein/keymap.c
+++ keyboards/lily58/keymaps/robrstein/keymap.c
@@ -172,7 +172,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
         case MACRO1:
             if (record->event.pressed) {
-                    SEND_STRING("853211utc");
+                    SEND_STRING("853211ip");
             }
             return false;
         case MACRO2:
